f77e204cf5fe42a8d32c9a7844ef391de131d4e3
Jacob Ng <jng1437@gmail.com>
2024-10-26 17:34:06 -0700

    Make worker run once, to see what information from the raw response is given.

--- crawler/worker.py
+++ crawler/worker.py
@@ -18,7 +18,8 @@ class Worker(Thread):
         super().__init__(daemon=True)
         
     def run(self):
-        while True:
+        count = 0
+        while count < 1:
             tbd_url = self.frontier.get_tbd_url()
             if not tbd_url:
                 self.logger.info("Frontier is empty. Stopping Crawler.")
@@ -32,3 +33,4 @@ class Worker(Thread):
                 self.frontier.add_url(scraped_url)
             self.frontier.mark_url_complete(tbd_url)
             time.sleep(self.config.time_delay)
+            count += 1
