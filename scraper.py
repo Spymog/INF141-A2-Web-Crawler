31676254e891b0ca799477e841427f71fb05c469
Jacob Ng <jng1437@gmail.com>
2024-10-26 17:43:02 -0700

    Change file write mode to write in bytes.

--- scraper.py
+++ scraper.py
@@ -16,7 +16,7 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    with open("out.txt", "w") as f:
+    with open("out.txt", "wb") as f:
         f.write(resp.raw_response.content)
     # print(resp.raw_response.content)
     return list()
