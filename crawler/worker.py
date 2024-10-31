1827b6935a311ca7219040181ff130ffe1311181
Jacob Ng <jng1437@gmail.com>
2024-10-31 12:38:56 -0700

    Changed worker back to run for it's original intended duration.

--- crawler/worker.py
+++ crawler/worker.py
@@ -18,8 +18,7 @@ class Worker(Thread):
         super().__init__(daemon=True)
         
     def run(self):
-        count = 0
-        while count < 1:
+        while True:
             tbd_url = self.frontier.get_tbd_url()
             if not tbd_url:
                 self.logger.info("Frontier is empty. Stopping Crawler.")
@@ -33,4 +32,3 @@ class Worker(Thread):
                 self.frontier.add_url(scraped_url)
             self.frontier.mark_url_complete(tbd_url)
             time.sleep(self.config.time_delay)
-            count += 1
