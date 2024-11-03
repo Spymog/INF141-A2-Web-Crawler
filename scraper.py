3f0054fdaa0a19cf1ba75b25876e1336b6c9143c
Jacob Ng <jng1437@gmail.com>
2024-11-02 22:46:48 -0700

    Added check for if scraped domains are within the domain range given. Added filter for stop words for counting words.

--- scraper.py
+++ scraper.py
@@ -35,51 +35,85 @@ def scraper(url, resp):
         print(f"Can't read url: '{url}',\nError code: {resp.status_code}") # Error message if status code isn't 200
         raise
     
-def process_raw_hyperlink(url, hyperlink):
-    parsed_url = urlparse(url)
-    parsed_link = urlparse(hyperlink)
 
-    if not parsed_link.scheme:
-        parsed_link = parsed_link._replace(scheme=parsed_url.scheme)
-    elif parsed_link.scheme not in set(["http", "https"]):
-        return
+# -- MAY NOT ACTUALLY NEED --
+# def process_raw_hyperlink(url, hyperlink):
+#     parsed_url = urlparse(url)
+#     parsed_link = urlparse(hyperlink)
+
+#     if not parsed_link.scheme:
+#         parsed_link = parsed_link._replace(scheme=parsed_url.scheme)
+#     elif parsed_link.scheme not in set(["http", "https"]):
+#         return
     
-    if not parsed_link.netloc:
+#     if not parsed_link.netloc:
 
-        if parsed_url.hostname in parsed_link.path:
-            new_path = parsed_link.path.replace(parsed_url.hostname, '', 1)
-            parsed_link = parsed_link._replace(netloc=parsed_url.hostname, path=new_path)
-        elif parsed_link.path.startswith('/'):
-            parsed_link = parsed_link._replace(netloc=parsed_url.hostname)
-        else:
-            current_path = parsed_url.hostname + '/' + parsed_url.path.lstrip('/')
-            parsed_link = parsed_link._replace(netloc=current_path)
-    else:
-        parsed_link = parsed_link._replace(netloc=parsed_link.hostname)
+#         if parsed_url.hostname in parsed_link.path:
+#             new_path = parsed_link.path.replace(parsed_url.hostname, '', 1)
+#             parsed_link = parsed_link._replace(netloc=parsed_url.hostname, path=new_path)
+#         elif parsed_link.path.startswith('/'):
+#             parsed_link = parsed_link._replace(netloc=parsed_url.hostname)
+#         else:
+#             current_path = parsed_url.hostname + '/' + parsed_url.path.lstrip('/')
+#             parsed_link = parsed_link._replace(netloc=current_path)
+#     else:
+#         parsed_link = parsed_link._replace(netloc=parsed_link.hostname)
 
-    if parsed_link.path == '/':
-        parsed_link = parsed_link._replace(path='')
+#     if parsed_link.path == '/':
+#         parsed_link = parsed_link._replace(path='')
 
-    parsed_link = urlparse(urldefrag(parsed_link.geturl())[0])
+#     parsed_link = urlparse(urldefrag(parsed_link.geturl())[0])
 
-    return parsed_link.geturl()
+#     return parsed_link.geturl()
+
 
 def find_longest(url:str, tokens:list):
+    '''
+    Compares given url and token list to current longest url in terms of words and it's word count
+
+    Takes the current url and a list of all tokens found at the current url as it's arguments.
+
+    Opens longest_page.txt, which is a 2 line txt file:
+        1. Number of tokens found at the longest url
+        2. The url itself
+    Checks if the length of the list of tokens arguement is greater than the recorded longest number of words from the current longest url.
+    If tokens argument is longer, write it's length and the name of the current url into longest_page.txt 
+    '''
     token_count = len(tokens)
 
     with open('answers/longest_page.txt', 'r') as current_longest:
         lines = current_longest.readlines()
         longest_count = int(lines[0])
-        longest_url = lines[1] # Maybe don't need
+        # longest_url = lines[1] # Maybe don't need
 
         if token_count > longest_count:
             with open('answers/longest_page.txt', 'w') as new_longest:
                 new_longest.write(f'{str(token_count)}\n{url}')
 
+
 def update_token_counts(tokens:list):
+    '''
+    Uses shelve to keep a persistent dict{word:count} of all the words and the current count of their occurences.
+
+    Takes a list of tokens as it's argument.
+
+    Opens the file word_counts which is used as the persistent dict and iterates through each token in the tokens list, updating the 
+    count of the corresponding key by if such a key exists, otherwise creates a new key with a count of 1.
+    '''
+
+    stop_words = "a about above after again against all am an and any are aren't as at be because been before being below between both \
+but by can't cannot could couldn't did didn't do does doesn't doing don't down during each few for from further had hadn't has hasn't \
+have haven't having he he'd he'll he's her here here's hers herself him himself his how how's i i'd i'll i'm i've if in into is isn't \
+it it's its itself let's me more most mustn't my myself no nor not of off on once only or other ought our ours ourselves out over own \
+same shan't she she'd she'll she's should shouldn't so some such than that that's the their theirs them themselves then there there's \
+these they they'd they'll they're they've this those through to too under until up very was wasn't we we'd we'll we're we've were \
+weren't what what's when when's where where's which while who who's whom why why's with won't would wouldn't you you'd you'll you're \
+you've your yours yourself yourselves"
+
     with shelve.open('answers/word_counts') as count:
         for token in tokens:
-            count[token] = count.get(token, 0) + 1
+            if token not in stop_words:
+                count[token] = count.get(token, 0) + 1
 
 
 def extract_next_links(url, resp):
@@ -97,7 +131,6 @@ def extract_next_links(url, resp):
     # Takes the raw response from the server and converts in into a Beautiful soup object, which can parse through the response and 
     # allows easy access to certain parts of the html. BeautifulSoup = important part of being able to parse through the html, 
     # but NOT the strings themselves
-    # tag = SoupStrainer('a')
     soup = BeautifulSoup(resp.raw_response.content, 'html.parser') 
 
     # Extract the text of the current webpage and convert to tokens
@@ -111,17 +144,18 @@ def extract_next_links(url, resp):
     update_token_counts(tokens)
 
     # Find all the a tags in the html, add them to the set of raw hyperlinks  
-    raw_hyperlinks = set() # Set of scraped potential hyperlinks, have to process them to make sure they are crawlable
+    raw_hyperlinks = set() 
     for tag in soup.find_all('a'):
-        hyperlink = tag.get('href') # Get the contents of the href attribute from each a tag
-        if hyperlink: # Check if there is any contents taken from the tag
-            raw_hyperlinks.add(hyperlink) # Add to set of raw hyperlinks
+        hyperlink = tag.get('href')
+        if hyperlink:
+            raw_hyperlinks.add(hyperlink)
 
+    # Convert raw hyperlinks to absolute links, add them all to a list to be returned
     absolute_links = list()
-    # Convert raw hyperlinks to absolute links
     for link in raw_hyperlinks:
         # processed_link = process_raw_hyperlink(url, link)
         absolute_link = urljoin(url, link)
+        absolute_link = urldefrag(absolute_link)[0] # Remove any fragments from the end 
         absolute_links.append(absolute_link)
         # TODO: write processed_link's URL only to a file so we can count it later
         # TODO: use the soup to get all tokens (across all pages), and keep track of the page with the most number of words
@@ -134,14 +168,34 @@ def extract_next_links(url, resp):
     
     return absolute_links
 
+
 def is_valid(url):
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
+    
+    # valid_domains = [
+    #     'ics.uci.edu/',
+    #     'cs.uci.edu/',
+    #     'informatics.uci.edu/',
+    #     'stat.uci.edu/',
+    #     'today.uci.edu/department/information_computer_sciences/'
+    #     ]
+
     try:
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
+        
+        path_parts = parsed.path.strip('/').split('/')
+        if len(path_parts) > 1 and len(path_parts) == len(set(path_parts)):
+            return False
+        
+        valid_domains = r'((\w*\.)*(ics|cs|informatics|stat)\.uci\.edu)|(today\.uci\.edu\/department\/information_computer_sciences)'
+
+        if not re.match(valid_domains, parsed.hostname + parsed.path):
+            return False
+        
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
