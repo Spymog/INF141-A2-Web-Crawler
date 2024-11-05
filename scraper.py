00d4a41f309dcd066f38959f8f6b44c2e0d70366
Jacob Ng <jng1437@gmail.com>
2024-11-04 16:19:07 -0800

    Updated some of the comments for various comments.

--- scraper.py
+++ scraper.py
@@ -33,7 +33,7 @@ def scraper(url, resp):
         links = extract_next_links(url, resp) # Get all links from current url
         return [link for link in links if is_valid(link)] # For each link, check if link will lead to a webpage, if so return it
     else:
-        print(f"Can't read url: '{url}',\nError code: {resp.status_code}") # Error message if status code isn't 200
+        print(f"Can't read url: '{url}',\nStatus code: {resp.status_code}") # Error message if status code isn't 200
         raise
     
 
@@ -92,7 +92,7 @@ def find_longest(url:str, tokens:list):
                 new_longest.write(f'{str(token_count)}\n{url}')
 
 
-def update_token_counts(tokens:list):
+def update_token_counts(tokens:list, stop_words):
     '''
     Uses shelve to keep a persistent dict{word:count} of all the words and the current count of their occurences.
 
@@ -101,16 +101,7 @@ def update_token_counts(tokens:list):
     Opens the file word_counts which is used as the persistent dict and iterates through each token in the tokens list, updating the 
     count of the corresponding key by if such a key exists, otherwise creates a new key with a count of 1.
     '''
-
-    stop_words = "a about above after again against all am an and any are aren't as at be because been before being below between both \
-but by can't cannot could couldn't did didn't do does doesn't doing don't down during each few for from further had hadn't has hasn't \
-have haven't having he he'd he'll he's her here here's hers herself him himself his how how's i i'd i'll i'm i've if in into is isn't \
-it it's its itself let's me more most mustn't my myself no nor not of off on once only or other ought our ours ourselves out over own \
-same shan't she she'd she'll she's should shouldn't so some such than that that's the their theirs them themselves then there there's \
-these they they'd they'll they're they've this those through to too under until up very was wasn't we we'd we'll we're we've were \
-weren't what what's when when's where where's which while who who's whom why why's with won't would wouldn't you you'd you'll you're \
-you've your yours yourself yourselves"
-
+    
     with shelve.open('answers/word_counts') as count:
         for token in tokens:
             if token not in stop_words:
@@ -142,7 +133,15 @@ def extract_next_links(url, resp):
     find_longest(url, tokens)
     
     # Count frequencies of all found tokens and update total count of all tokens
-    update_token_counts(tokens)
+    stop_words = "a about above after again against all am an and any are aren't as at be because been before being below between both \
+        but by can't cannot could couldn't did didn't do does doesn't doing don't down during each few for from further had hadn't has hasn't \
+        have haven't having he he'd he'll he's her here here's hers herself him himself his how how's i i'd i'll i'm i've if in into is isn't \
+        it it's its itself let's me more most mustn't my myself no nor not of off on once only or other ought our ours ourselves out over own \
+        same shan't she she'd she'll she's should shouldn't so some such than that that's the their theirs them themselves then there there's \
+        these they they'd they'll they're they've this those through to too under until up very was wasn't we we'd we'll we're we've were \
+        weren't what what's when when's where where's which while who who's whom why why's with won't would wouldn't you you'd you'll you're \
+        you've your yours yourself yourselves"
+    update_token_counts(tokens, stop_words)
 
     # Find all the a tags in the html, add them to the set of raw hyperlinks  
     raw_hyperlinks = set() 
@@ -181,25 +180,27 @@ def is_valid(url):
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
     
-    # valid_domains = [
-    #     'ics.uci.edu/',
-    #     'cs.uci.edu/',
-    #     'informatics.uci.edu/',
-    #     'stat.uci.edu/',
-    #     'today.uci.edu/department/information_computer_sciences/'
-    #     ]
+    # valid_domains:
+    # *.ics.uci.edu/*
+    # *.cs.uci.edu/*
+    # *.informatics.uci.edu/*
+    # *.stat.uci.edu/*
+    # today.uci.edu/department/information_computer_sciences/*
 
     try:
+        # Checks if the link has a correct scheme, either http or https
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
         
+        # Gets the link's path and checks to see if it is a repeating pattern
         path_parts = parsed.path.strip('/').split('/')
         if len(path_parts) > 1 and len(path_parts) == len(set(path_parts)):
             return False
-        
+
         valid_domains = r'((\w*\.)*(ics|cs|informatics|stat)\.uci\.edu)|(today\.uci\.edu\/department\/information_computer_sciences)'
 
+        # Checks to see if domain and path are within the domain range for the assignment
         if not re.match(valid_domains, parsed.hostname + parsed.path):
             return False
         
